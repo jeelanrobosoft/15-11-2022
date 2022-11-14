3479f83e94216971ebe390820d4031c116048eb7
AK_Jeelan <jeelan.ak@robosoft.in>
2022-11-14 19:30:00 +0530

    sixth commit

--- src/main/java/com/robosoft/virtualLearn/register_login/register/sms/service/SmsService.java
+++ src/main/java/com/robosoft/virtualLearn/register_login/register/sms/service/SmsService.java
@@ -16,7 +16,7 @@ public class SmsService {
     DataAccessService dataAccessService;
 
     private final static String ACCOUNT_SID = "ACd7b80d5a6e82ec89f4be4cc8779fd230";
-    private final static String AUTH_ID = "d51fdd3ddccd326261fc1ed7064c045cgit ";
+    private final static String AUTH_ID = "d51fdd3ddccd326261fc1ed7064c045c";
 
     static {
         Twilio.init(ACCOUNT_SID, AUTH_ID);
